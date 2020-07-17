521bdf2b2bca6ff6ddb1cd19c33a9e9430c7f26d
Maronato <gustavomaronato@gmail.com>
2020-07-16 22:05:19 -0300

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,26 @@
+<template>
+  <h1>{{ msg }}</h1>
+  <div>Dot env variable: {{ dotEnv ? dotEnv : "undefined" }}</div>
+  <br>
+  <div>Cross env variable: {{ crossEnv ? crossEnv : "undefined" }}</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  },
+  data() {
+    return {
+      count: 0
+    }
+  },
+  setup() {
+    const dotEnv = import.meta.env.VITE_DOT_ENV;
+    const crossEnv = import.meta.env.VITE_CROSS_ENV;
+
+    return { dotEnv, crossEnv }
+  }
+}
+</script>
